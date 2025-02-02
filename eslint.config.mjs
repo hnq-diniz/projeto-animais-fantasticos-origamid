0c523ecebe1fb71f9436dfc17afee9361071ef6e
hnq-diniz <henrique.diniz68@yahoo.com.br>
2025-02-02 19:08:39 -0300

    primeito commit

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import { javascript } from 'webpack';
+
+export default [
+  {
+    rules: {
+      // 'no-console': 'error',
+      indent: ['error', 2],
+      'keyword-spacing': 'error',
+      'no-multiple-empty-lines': 'error',
+      'eol-last': ['error', 'always'],
+      semi: ['error', 'always'],
+      'no-trailing-spaces': 'error',
+      'operator-assignment': 'error',
+      'no-inner-declarations': ['error', 'functions', { blockScopedFunctions: 'disallow' }],
+    },
+  },
+];
